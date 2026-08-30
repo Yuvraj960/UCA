5987e0cecb9c6bb26b99617ada329868047b9c71
Yuvraj Gupta <yuvrajgupta956@gmail.com>
2026-08-30 22:52:58 +0530

    Implemented Base BST Structure

--- Problem Solving/Binary Search Trees/BST.java
+++ Problem Solving/Binary Search Trees/BST.java
@@ -1,153 +1,193 @@
-public class BST<Key extends Comparable<Key>, Value> {
-  private Node root;
+import java.util.ArrayList;
+import java.util.List;
 
-  private class Node {
-    Key key;
-    Value val;
-    Node left;
-    Node right;
-    int N; // No. of nodes in subtree rooted here
-    Node(Key key, Value val) {
-      this.key = key;
-      this.val = val;
-      this.N = 1;
+public class BST<K extends Comparable<K>, V> {
+    private Node root;
+
+    public int height() {
+        return height(root);
     }
-  }
 
-  // Min key
-  public Key min() {
-    if (root == null) return null; // Tree is empty
-    Node x = root;
-    while (x.left != null) {
-        x = x.left;
+    private int height(Node n) {
+        if (n == null) return 0;
+        return 1 + Math.max(height(n.left), height(n.right));
     }
-    return x.key;
-  }
 
-  //Function to delete the min
-  public void delMin() {
-    root = delMin(root);
-  }  
-  
-  private Node delMin(Node x) {
-    if (x.left == null) return x.right;
-    x.left = delMin(x.left);
-    return x; 
-  }
-
-  // Max key
-  public Key max() {
-    if (root == null) return null; // Tree is empty
-    Node x = root;
-    while (x.right != null) {
-        x = x.right;
+    public void convertToDLink() {
+        if (root != null) {
+            List<Node> ends = convertToDLink(root);
+            if (ends.size() == 2) {
+                ends.get(0).left = ends.get(1);
+                ends.get(1).right = ends.get(0);
+                root = ends.get(0);
+            }
+        }
     }
-    return x.key;
-  }
-  
-  // Floor - largest number just smaller than given
-  public Key floor(Key key) {
-    Node x = floor(root, key);
-    if (x == null) return null;
-    return x.key;
-  }
 
-  private Node floor(Node x, Key key) {
-    if (x == null) return null;
+    private List<Node> convertToDLink(Node n) {
+        List<Node> ends = new ArrayList<>(2);
+        if (n != null) {
+            List<Node> lEnds = convertToDLink(n.left);
+            List<Node> rEnds = convertToDLink(n.right);
+            if (lEnds.isEmpty()) {
+                ends.add(n);
+            } else {
+                ends.add(lEnds.get(0));
+                Node t = lEnds.get(lEnds.size() - 1);
+                t.right = n;
+                n.left = t;
+            }
+            if (rEnds.isEmpty()) {
+                if (!lEnds.isEmpty()) {
+                    ends.add(n);
+                }
+            } else {
+                rEnds.get(0).left = n;
+                n.right = rEnds.get(0);
+                Node t = rEnds.get(rEnds.size() - 1);
+                ends.add(t);
+            }
+        }
+        return ends;
+    }
 
-    int cmp = key.compareTo(x.key);
-    if (cmp == 0) return x; // Exact match
-    if (cmp < 0) return floor(x.left, key); // Search left
+    public K fca(K k1, K k2) {
+        if (search(root, k1) == null || search(root, k2) == null) return null;
+        return fca(root, k1, k2, null);
+    }
 
-    Node t = floor(x.right, key); // Search right
-    if (t != null) return t;
-    else return x;
-  }
-  
-  // Ceil - Smallest number just greater than given
-  public Key ceil(Key key) {
-    Node x = ceil(root, key);
-    if (x == null) return null;
-    return x.key;
-  }
+    private K fca(Node n, K k1, K k2, Node p) {
+        int comp1 = k1.compareTo(n.k);
+        int comp2 = k2.compareTo(n.k);
+        if ((comp2 < 0 && comp1 > 0) || (comp2 > 0 && comp1 < 0)) return n.k;
+        if (comp2 > 0 && comp1 > 0) return fca(n.right, k1, k2, n);
+        if (comp2 < 0 && comp1 < 0) return fca(n.left, k1, k2, n);
+        return p == null ? null : p.k; // k1 == n.k or k2 == n.k
+    }
 
-  private Node ceil(Node x, Key key) {
-    if (x == null) return null;
+    public void insert(K k, V v) {
+        root = insert(root, k, v);
+    }
 
-    int cmp = key.compareTo(x.key);
-    if (cmp == 0) return x; // Exact match
-    if (cmp > 0) return ceil(x.right, key); // Search right
+    private Node insert(Node n, K k, V v) {
+        if (n == null) return new Node(k, v);
+        int comp = k.compareTo(n.k);
+        if (comp > 0) {
+            n.right = insert(n.right, k, v);
+        } else if (comp < 0) {
+            n.left = insert(n.left, k, v);
+        } else {
+            n.v = v; //update
+        }
+        return n;
+    }
 
-    Node t = ceil(x.left, key); // Search left
-    if (t != null) return t;
-    else return x;
-  }
+    public V search(K k) {
+        Node t = search(root, k);
+        return t == null ? null : t.v;
+    }
 
-  public int rank(Key key) {
-    Value v = get(key);
-    if (v == null) return -1;
-    return rank(root, key);
-  }
-  
-  private int rank(Node x, Key k) {
-    int cmp = k.compareTo(x.key);
-    if (cmp == 0) return sizeof(x.left);
-    if (cmp < 0) return rank(x.left, k);
-    return 1 + sizeof(x.left) + rank(x.right, k);
-  }
+    private Node search(Node n, K k) {
+        if (n == null) return null;
+        int comp = k.compareTo(n.k);
+        if (comp > 0) {
+            return search(n.right, k);
+        }
+        if (comp < 0) {
+            return search(n.left, k);
+        }
+        return n;
+    }
 
-  // Get method
-  public Value get(Key key) {
-    return get(root, key);
-  }
 
-  private int sizeof(Node x) {
-    return x == null ? 0 : x.N;
-  }
-  
-  private Value get(Node x, Key key) {
-    if (x == null) return null;
-    
-    int cmp = key.compareTo(x.key);
+    public K getMin() {
+        if (root == null) return null;
+        Node n = getMin(root);
+        return n.k;
+    }
 
-    if (cmp == 0) return x.val;
-    else if (cmp < 0) return get(x.left, key);
-    else return get(x.right, key);
-  }      
-  
-  // Put method
-  public void put(Key key, Value val) {
-    root = put(root, key, val);
-  }
-  
-  private Node put(Node x, Key key, Value val) {
-    if (x == null) return new Node(key, val);
+    private Node getMin(Node n) {
+        if (n.left == null) {
+            return n;
+        }
+        return getMin(n.left);
+    }
 
-    int cmp = key.compareTo(x.key);
-    
-    if (cmp == 0) x.val = val; //overrite the key
-    else if (cmp < 0) x.left = put(x.left, key, val);
-    else x.right = put(x.right, key, val);
-    x.N += sizeof(x.left) + sizeof(x.right);
-    return x;
-  }
+    public void delMin() {
+        if (root == null) return;
+        root = delMin(root);
+    }
 
-  public static void main(String[] args) {
-    BST<Integer, String> bst = new BST<Integer, String>();
-    
-    bst.put(5, "A");
-    bst.put(1, "X");
-    bst.put(3, "Y");
-    bst.put(4, "Z");
-    bst.put(7, "A");
+    private Node delMin(Node n) {
+        if (n.left == null) {
+            return n.right;
+        }
+        n.left = delMin(n.left);
+        return n;
+    }
 
-    assert bst.get(5).equals("A") : "Test failed for key 5";
-    assert bst.get(1).equals("X") : "Test failed for key 5";
-    assert bst.get(3).equals("Y") : "Test failed for key 5";
-    assert bst.get(4).equals("Z") : "Test failed for key 5";
-    assert bst.get(7).equals("A") : "Test failed for key 5";
-    assert bst.get(10) == null : "Test failed for key 10 , key missing";
-    
-    System.out.println("All test cases passed successfully");
-  }
+    public void del(K k) {
+        root = del(root, k);
+    }
+
+    private Node del(Node n, K k) {
+        if (n == null) return null;
+        int comp = k.compareTo(n.k);
+        if (comp > 0) {
+            n.right = del(n.right, k);
+        } else if (comp < 0) {
+            n.left = del(n.left, k);
+        } else {
+            if (n.left == null) {
+                //case 1 and 2
+                return n.right;
+            }
+            if (n.right == null) {
+                //case 3
+                return n.left;
+            }
+            //case 4
+            Node t = getMin(n.right); // t has not left but some right
+            t.right = delMin(n.right);
+            t.left = n.left;
+            return t;
+        }
+        return n;
+    }
+
+    public void printLinkedList() {
+        StringBuilder sb = new StringBuilder();
+        Node t = root;
+        do {
+            sb.append(t.k).append("(").append(t.v).append(")").append("->");
+            t = t.right;
+        } while (t.k != root.k);
+        sb.append(root.k).append("(").append(root.v).append(")");
+        System.out.println(sb);
+
+        sb = new StringBuilder();
+        t = root;
+        do {
+            sb.append(t.k).append("(").append(t.v).append(")").append("->");
+            t = t.left;
+        } while (t.k != root.k);
+        sb.append(root.k).append("(").append(root.v).append(")");
+        System.out.println(sb);
+    }
+
+    private class Node {
+        private final K k;
+        private V v;
+
+        private Node left;
+        private Node right;
+
+        public Node(K k, V v) {
+            this.k = k;
+            this.v = v;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
 }
